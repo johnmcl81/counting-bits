432027743c076556659a143c8638b244c6e18732
johnmcl81 <j.d.mclachlan@gmail.com>
2018-03-11 14:19:34 +0100

    performance testing

--- PositiveBitCounter.js
+++ PositiveBitCounter.js
@@ -3,17 +3,18 @@
 // CAVEAT: Bitwise operators only work for a maximum of 32 bits
 
 function Count (input) {
+  console.time('performance')
   if (isRangeError(input)) {
     throw new RangeError('Error: Input must be positive integer')
   } else {
-    var count = 0
-    var number = input
-    var positionArray = []
+    let count = 0
+    let number = input
+    let positionArray = []
     if (number > 0) {
       // Loop while input number not 0
       while (number > 0) {
         // get position of set bit and add to positionArray
-        var bitPostion = Math.log2(number ^ (number & (number - 1)))
+        let bitPostion = Math.log2(number ^ (number & (number - 1)))
         positionArray.push(bitPostion)
 
         // right most poitive bit is set to 0 and count incremented
@@ -24,7 +25,7 @@ function Count (input) {
 
     // add postive bit count to front of array
     positionArray.unshift(count)
-
+    console.timeEnd('performance')
     return positionArray
   }
 }
